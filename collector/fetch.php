602016b7575722654007d0c03e038b4842adadb9
Sebastian Nohn <sebastian.nohn@ligatus.com>
2014-04-15 18:55:44 +0200

    if ext/openssl is not available use phpseclib

--- collector/fetch.php
+++ collector/fetch.php
@@ -3,6 +3,18 @@
 
 require_once '../config.php';
 
+$key = file_get_contents('../keys/'.$identity.'.pem');
+
+if (extension_loaded('openssl')) {
+    $key_id = openssl_get_privatekey($key);
+} else {
+    require_once 'Crypt/RSA.php';
+    $rsa = new Crypt_RSA();
+    $key = file_get_contents('../keys/'.$identity.'.pem');
+    $rsa->loadKey($key);
+    $rsa->setSignatureMode(CRYPT_RSA_SIGNATURE_PKCS1);
+}
+
 foreach ($objects as $object_name => $object_data) {
     foreach ($sensors as $sensor_name => $sensor_data) {
         $sensor_file = sha1($sensor_name . $object_name);
@@ -12,27 +24,42 @@ foreach ($objects as $object_name => $object_data) {
             'identity' => $identity,
             'time' => time(),
             'string' => $object_data['string']);
-
         $http_query = http_build_query($parameters);
-        $key = file_get_contents('../keys/'.$identity.'.pem');
-        $key_id = openssl_get_privatekey($key);
-        openssl_sign($http_query, $signature, $key);
-        openssl_free_key($key_id);
+        if (extension_loaded('openssl')) {
+            openssl_sign($http_query, $signature, $key);
+        } else {
+            $signature = $rsa->sign($http_query);
+        }
+        $signature = $rsa->sign($http_query);
         $signature = rtrim(strtr(base64_encode($signature), '+/', '-_'), '=');
         $parameters['signature'] = $signature;
         $http_query = http_build_query($parameters);
         $sensor_url = $sensor_data['url'] . '?' . $http_query;
-        if (!$response = file_get_contents($sensor_url)) {
-            throw new Exception('Can\'t open ' . $sensor_url);
+	$perfdata_tmp = array();
+	for ($i=0; $i < $samples; $i++) {
+            try {
+                if (!$response = file_get_contents($sensor_url)) {
+                    throw new Exception('Can\'t open ' . $sensor_url);
+                }
+                if (!$json = json_decode($response)) {
+                    throw new Exception('Can\'t decode. Sensory Node Response: ' . $response);
+                }
+                if (isset($json->performance->time_total_millis)) {
+                    $perfdata_tmp[] = (int) $json->performance->time_total_millis;
+                } else {
+                    $perfdata_tmp[] = 0;
+                }
+            } catch (Exception $e) {
+                echo $e->getMessage();
+            }
         }
-        if (!$json = json_decode($response)) {
-            throw new Exception('Can\'t decode. Sensory Node Response: ' . $response);
-        }
-        if (isset($json->performance->time_total_millis)) {
-            $perfdata = (int) $json->performance->time_total_millis;
-        } else {
-            $perfdata = 0;
-        }
+
+	// get the median
+        rsort($perfdata_tmp);
+        $middle = round(count($perfdata_tmp) / 2);
+        $perfdata = $perfdata_tmp[$middle-1];
+	unset($perfdata_tmp);
+
         file_put_contents('/tmp/perfboard_' . $sensor_file, $perfdata);
     }
 }
